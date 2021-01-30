b857d395a014a5ba0787c1decc109c9aa41f0109
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-01-31 01:22:05 +0200

    Выполнила задачу 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,5 @@
+const ingredientsList = document.querySelector('#ingredients');
+
 const ingredients = [
   'Картошка',
   'Грибы',
@@ -6,3 +8,13 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+
+const ingredientsAttachedItem = ingredients.map(ingredient => {
+  const ingredientsListItem = document.createElement('li');
+  ingredientsListItem.textContent = ingredient;
+
+  return ingredientsListItem;
+})
+
+ingredientsList.append(...ingredientsAttachedItem);
