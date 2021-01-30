5fdb195280dff2683aeb3a59cffab5ad552ba450
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-01-30 23:08:40 +0200

    Выполнила задачу 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,9 @@
-const categories = document.querySelector('#categories')
+const caregoryItems = document.querySelectorAll('.item');
+
+console.log(`В списке ${caregoryItems.length} категории.`);
+
+caregoryItems.forEach((item) => {
+  console.log(`Категория: ${item.querySelector("h2").textContent}`);
+  console.log(`Количество элементов: ${item.querySelectorAll("li").length}`);
+});
+
